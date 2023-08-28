3ae2bf02c9f09e412fbf34c1c4265b70579b4561
takeshiemoto <private.takeshiemoto@gmail.com>
2023-08-28 23:24:45 +0900

    init

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,33 @@
-import { sayHello } from "./index";
+import { decrement, increment, sayHello, sum } from "./index";
+
+beforeEach(() => {
+  console.log("beforeEach");
+});
+
+afterEach(() => {
+  console.log("afterEach");
+});
+
+beforeAll(() => {
+  console.log("beforeAll");
+});
+
+afterAll(() => {
+  console.log("afterAll");
+});
 
 test("say hello", () => {
   expect(sayHello()).toBe("Hello!");
+});
+
+test("sum", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test("increment", () => {
+  expect(increment(1)).toBe(2);
+});
+
+test("decrement", () => {
+  expect(decrement(1)).toBe(0);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 export const sayHello = () => {
   return "Hello!";
 };
+
+export const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+export const increment = (a: number) => {
+  return a + 1;
+};
+
+export const decrement = (a: number) => {
+  return a - 1;
+};
